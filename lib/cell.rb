21f391ae9239923f4fb506fa6357a0c2853919de
Joe King <j.king92@gmail.com>
2022-10-18 10:20:28 -0700

    feat: Add methods ship and empty

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,13 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
   def initialize(location)
     @coordinate = location
+    @ship = nil
+  end
+
+  def empty?
+    @ship == nil
   end
 
 end
