c14dc83e2e9839962ffb654fc203a5e6a875f47e
Joe King <j.king92@gmail.com>
2022-10-18 10:38:08 -0700

    test: Add tests for fired_upon? and fire_upon methods

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -46,6 +46,38 @@ describe Cell do
 
       expect(cell.ship).to be_a Ship
     end
+  end
 
+  describe '#fired_upon?' do
+    it 'confirms if it has not been fired upon' do
+      cell = Cell.new("C3")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+
+      expect(cell.fired_upon?).to eq false
+    end
+
+    it 'confirms if it has been fired upon' do
+      cell = Cell.new("C3")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      cell.fire_upon
+
+      expect(cell.fired_upon).to eq true
+    end
+  end
+
+  describe '#fire_upon' do
+    it 'makes the ship in the cell take a hit' do
+      cell = Cell.new("C3")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+
+      expect(cell.ship.health).to eq 3
+
+      cell.fire_upon 
+
+      expect(cell.ship.health).to eq 2
+    end
   end
 end
