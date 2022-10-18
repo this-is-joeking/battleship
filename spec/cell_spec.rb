807b1119476e8fd70a8cc2de47bbc28ecac8a512
Joe King <j.king92@gmail.com>
2022-10-18 09:21:21 -0700

    test: add initial tests for cell class

---
+++ spec/cell_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+
+describ Cell do
+  #Test the class exists and can be initiated
+  it 'exists' do
+    cell = Cell.new("A3")
+
+    expect(cell).to be_a Cell
+  end
+
+  describe '#coordinate' do
+    it 'returns the assigned coordinate' do
+      cell = Cell.new("B1")
+
+      expect(cell.coordinate).to eq("B1")
+    end
+  end
+
+end
