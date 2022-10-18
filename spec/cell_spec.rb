aec9459ee917948907cff3843ada27b234c469f7
Joe King <j.king92@gmail.com>
2022-10-18 10:19:07 -0700

    test: Add tests for ship and empty method

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -10,12 +10,22 @@ describe Cell do
     expect(cell).to be_a Cell
   end
 
-  describe '#coordinate' do
-    it 'returns the assigned coordinate' do
-      cell = Cell.new("B1")
+  it 'returns the assigned coordinate' do
+    cell = Cell.new("B1")
 
-      expect(cell.coordinate).to eq("B1")
-    end
+    expect(cell.coordinate).to eq("B1")
+  end
+
+  it 'starts as an empy cell not holding a ship' do
+    cell = Cell.new("D2")
+
+    expect(cell.ship).to eq nil
+  end
+
+  it 'can confirm if the cell is empty' do
+    cell = Cell.new("D2")
+
+    expect(cell.empty?).to eq true
   end
 
 end
