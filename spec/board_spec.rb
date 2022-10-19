99d2ebb11c20f2ec1255c9d39f4098860be1ed9c
Joe King <j.king92@gmail.com>
2022-10-18 20:31:16 -0700

    test: Check if the board is holding hash of 16 Cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,4 +10,11 @@ describe Board do
     expect(board).to be_a Board
   end
 
+  it 'holds a hash of 16 cards' do
+    board = Board.new
+
+    expect(board.cells.length).to eq 16
+    expect(board.cells["A1"]).to be_a Cell
+    expect(board.cells["D4"]).to be_a Cell
+  end
 end
