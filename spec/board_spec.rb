977283ee8d764ab4c6fce88c390d10cfa7690e65
Joe King <j.king92@gmail.com>
2022-10-18 20:13:21 -0700

    test: Add initial test for Board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+describe Board do
+  it 'exists' do
+    board = Board.new
+
+    expect(board).to be_a Board
+  end
+
+end
