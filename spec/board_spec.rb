66f92c2d6cb9c452a6b31a32564a0ab69fef7101
Joe King <j.king92@gmail.com>
2022-10-20 13:33:00 -0700

    feat: Improve #valid_placement method to make sure ships do not overlap

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,7 +9,7 @@ describe Board do
 
     expect(board).to be_a Board
   end
-
+  # tests the cell_maker
   it 'holds a hash of 16 cells' do
     board = Board.new
 
@@ -19,84 +19,101 @@ describe Board do
     expect(board.cells["D4"]).to be_a Cell
   end
 
-  it 'validates a coordinate' do
-    board = Board.new
-    board.cells
+  describe '#valid_coordinate?' do
+    it 'validates a coordinate' do
+      board = Board.new
+      board.cells
 
-    expect(board.valid_coordinate?("A1")).to be true
-    expect(board.valid_coordinate?("D4")).to be true
-    expect(board.valid_coordinate?("A5")).to be false
-    expect(board.valid_coordinate?("E1")).to be false
+      expect(board.valid_coordinate?("A1")).to be true
+      expect(board.valid_coordinate?("D4")).to be true
+      expect(board.valid_coordinate?("A5")).to be false
+      expect(board.valid_coordinate?("E1")).to be false
+    end
+  end
+  describe '#valid_placement?' do
+    it 'validates length of the ship' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      board.cells
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
+      expect(board.valid_placement?(submarine, ["A2", "A3"])).to be true
+    end
+
+    it 'validates coordinates are consecutive' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      board.cells
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+      expect(board.valid_placement?(submarine, ["A2", "C1"])).to be false
+      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+      expect(board.valid_placement?(submarine, ["B1", "B2"])).to be true
+    end
+
+    it 'does not let coordinates be diagonal' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      board.cells
+
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
+    end
+
+    it 'confirm placements are valid' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+    end
+
+    it 'checks to see if there is already ship in the cell(s)' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to be true
+      board.place(cruiser, ['A1', 'A2', 'A3'])
+
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be false
+      expect(board.valid_placement?(submarine, ["B1", "B2"])).to be true
+    end
   end
 
-  it 'validates length of the ship' do
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    board.cells
+  describe '#place' do
+    it 'can place a ship on cells' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
 
-    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
-    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
-    expect(board.valid_placement?(submarine, ["A2", "A3"])).to be true
-  end
+      board.cell_maker
+      board.place(cruiser, ["A1", "A2", "A3"])
+      cell_1 = board.cells["A1"]
+      cell_2 = board.cells["A2"]
+      cell_3 = board.cells["A3"]
 
-  it 'validates coordinates are consecutive' do
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    board.cells
+      expect(cell_1.ship).to be_a(Ship)
+      expect(cell_2.ship).to be_a(Ship)
+      expect(cell_3.ship).to be_a(Ship)
+    end
 
-    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
-    expect(board.valid_placement?(submarine, ["A2", "C1"])).to be false
-    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
-    expect(board.valid_placement?(submarine, ["B1", "B2"])).to be true
-  end
+    it 'can hold same ship in multiple cells' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
 
-  it 'does not let coordinates be diagonal' do
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    board.cells
+      board.cells
+      board.place(cruiser, ["A1", "A2", "A3"])
+      cell_1 = board.cells["A1"]
+      cell_2 = board.cells["A2"]
+      cell_3 = board.cells["A3"]
 
-    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
-    expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
-  end
-
-  it 'confirm placements are valid' do
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    board.cells
-
-    expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
-    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
-  end
-
-  it 'can place a ship on cells' do
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-
-    board.cell_maker
-    board.place(cruiser, ["A1", "A2", "A3"])
-    cell_1 = board.cells["A1"]
-    cell_2 = board.cells["A2"]
-    cell_3 = board.cells["A3"]
-
-    expect(cell_1.ship).to be_a(Ship)
-    expect(cell_2.ship).to be_a(Ship)
-    expect(cell_3.ship).to be_a(Ship)
-  end
-
-  it 'can hold same ship in multiple cells' do
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-
-    board.cells
-    board.place(cruiser, ["A1", "A2", "A3"])
-    cell_1 = board.cells["A1"]
-    cell_2 = board.cells["A2"]
-    cell_3 = board.cells["A3"]
-
-    expect(cell_3.ship == cell_2.ship).to eq(true)
+      expect(cell_3.ship == cell_2.ship).to eq(true)
+    end
   end
 end
