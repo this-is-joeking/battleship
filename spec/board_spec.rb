6a2a1631a66555b32632421c73109893bb313729
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-20 12:07:24 -0700

    feat: Add place method to board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -76,14 +76,27 @@ describe Board do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
 
+    board.cell_maker
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+
+    expect(cell_1.ship).to be_a(Ship)
+    expect(cell_2.ship).to be_a(Ship)
+    expect(cell_3.ship).to be_a(Ship)
+  end
+
+  it 'can hold same ship in multiple cells' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
     board.cells
     board.place(cruiser, ["A1", "A2", "A3"])
     cell_1 = board.cells["A1"]
     cell_2 = board.cells["A2"]
     cell_3 = board.cells["A3"]
-    require 'pry'; binding.pry
-    expect(cell_1.ship).to be_a(Ship)
-    expect(cell_2.ship).to be_a(Ship)
-    expect(cell_3.ship).to be_a(Ship)
+
+    expect(cell_3.ship == cell_2.ship).to eq(true)
   end
 end
