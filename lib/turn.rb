6cd26a940cb2c6949c6a4d77773293b8bcf845ba
Joe King <j.king92@gmail.com>
2022-10-21 14:40:09 -0700

    feat: Add player_fires method to turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,19 @@
 class Turn
-
+  attr_reader :comp_board,
+              :player_board
+              
   def initialize(cell, comp_board, player_board)
     @cell = cell
     @comp_board = comp_board
     @player_board = player_board
   end
 
-  def computer_fires
-    @comp_board.array_of_coordinates.shuffle!.pop
+  def computer_aims
+    @player_board.array_of_coordinates.shuffle!.pop
   end
 
-  
+  def player_fires
+    @comp_board.cells[@cell].fire_upon
+  end
+
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -18,13 +18,33 @@ describe Turn do
     player_board = Board.new
     turn = Turn.new("A1", comp_board, player_board)
 
-    expect(turn.computer_fires).to be_a String
+    expect(turn.computer_aims).to be_a String
   end
 
   it 'can take the players shot' do
+    comp_board = Board.new
+    player_board = Board.new
+    turn = Turn.new("A1", comp_board, player_board)
 
-
+    expect(turn.comp_board.cells["A1"].view).to eq(".")
+    turn.player_fires
+    expect(turn.comp_board.cells["A1"].view).to eq("M")
   end
+
+  it 'can hit a ship on the board' do
+    comp_board = Board.new
+    player_board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    comp_board.place(cruiser, ["A1", "A2", "A3"])
+    comp_board.place(submarine, ["C3", "D3"])
+    turn = Turn.new("A1", comp_board, player_board)
+
+    expect(turn.comp_board.cells["A1"].view).to eq(".")
+    turn.player_fires
+    expect(turn.comp_board.cells["A1"].view).to eq("H")
+  end
+
 end
 
 
