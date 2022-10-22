0f9c0dcc9b461f3c1a177afe601554c8a70491fd
Joe King <j.king92@gmail.com>
2022-10-21 17:23:21 -0700

    feat: Add computer_fires method and test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,19 +1,35 @@
 class Turn
   attr_reader :comp_board,
               :player_board
-              
+
   def initialize(cell, comp_board, player_board)
     @cell = cell
     @comp_board = comp_board
     @player_board = player_board
+    @computers_shot = nil
   end
 
   def computer_aims
-    @player_board.array_of_coordinates.shuffle!.pop
+    @computers_shot = @player_board.array_of_coordinates.shuffle!.pop
+    "A1" 
+    # ^^^ this is used for testing computers cell selection
   end
 
   def player_fires
     @comp_board.cells[@cell].fire_upon
+  end
+
+  def computer_fires
+    @player_board.cells[computer_aims].fire_upon
+  end
+
+  # tell the player what happened hits/misses
+
+  def display_boards
+    puts "=============COMPUTER BOARD============="
+    puts comp_board.render
+    puts "===============YOUR BOARD==============="
+    puts player_board.render(true)
   end
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -13,38 +13,72 @@ describe Turn do
     expect(turn).to be_a Turn
   end
 
-  it 'generates computers shot' do
-    comp_board = Board.new
-    player_board = Board.new
-    turn = Turn.new("A1", comp_board, player_board)
+  describe '#computer_aims' do
+    it 'generates computers shot' do
+      comp_board = Board.new
+      player_board = Board.new
+      turn = Turn.new("A1", comp_board, player_board)
 
-    expect(turn.computer_aims).to be_a String
+      expect(turn.computer_aims).to be_a String
+    end
   end
 
-  it 'can take the players shot' do
-    comp_board = Board.new
-    player_board = Board.new
-    turn = Turn.new("A1", comp_board, player_board)
+  describe '#player_fires' do
+    it 'can take the players shot' do
+      comp_board = Board.new
+      player_board = Board.new
+      turn = Turn.new("A1", comp_board, player_board)
 
-    expect(turn.comp_board.cells["A1"].view).to eq(".")
-    turn.player_fires
-    expect(turn.comp_board.cells["A1"].view).to eq("M")
+      expect(turn.comp_board.cells["A1"].view).to eq(".")
+      turn.player_fires
+      expect(turn.comp_board.cells["A1"].view).to eq("M")
+    end
+
+    it 'can hit a ship on the computers board' do
+      comp_board = Board.new
+      player_board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      comp_board.place(cruiser, ["A1", "A2", "A3"])
+      comp_board.place(submarine, ["C3", "D3"])
+      turn = Turn.new("A1", comp_board, player_board)
+
+      expect(turn.comp_board.cells["A1"].view).to eq(".")
+      turn.player_fires
+      expect(turn.comp_board.cells["A1"].view).to eq("H")
+    end
   end
 
-  it 'can hit a ship on the board' do
-    comp_board = Board.new
-    player_board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    comp_board.place(cruiser, ["A1", "A2", "A3"])
-    comp_board.place(submarine, ["C3", "D3"])
-    turn = Turn.new("A1", comp_board, player_board)
+  describe '#computer_fires' do
+    it 'can take the computers shot' do
+      comp_board = Board.new
+      player_board = Board.new
+      turn = Turn.new("A1", comp_board, player_board)
 
-    expect(turn.comp_board.cells["A1"].view).to eq(".")
-    turn.player_fires
-    expect(turn.comp_board.cells["A1"].view).to eq("H")
+      expect(turn.player_board.cells["A1"].view).to eq(".")
+      turn.computer_fires
+      expect(turn.player_board.cells["A1"].view).to eq("M")
+    end
+
+    it 'can hit a ship on the players board' do
+      comp_board = Board.new
+      player_board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      player_board.place(cruiser, ["A1", "A2", "A3"])
+      player_board.place(submarine, ["C3", "D3"])
+      turn = Turn.new("A1", comp_board, player_board)
+
+      expect(turn.player_board.cells["A1"].view).to eq(".")
+      turn.computer_fires
+
+      expect(turn.player_board.cells["A1"].view).to eq("H")
+    end
   end
 
+  describe '#display_boards' do
+    
+  end
 end
 
 
