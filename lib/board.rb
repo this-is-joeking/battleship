1711c842ab5409ede55e7faa484a02bb44f208fb
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-24 13:44:40 -0700

    test: Add tests for new board methods

--- lib/board.rb
+++ lib/board.rb
@@ -7,26 +7,31 @@ attr_reader :cells,
     @board_height = 4
     @board_width = 4
     @array_of_coordinates = []
-    @width_nums = []
-    @length_letters = []
     cell_maker
   end
 
+  def alphabet
+    ('A'..'Z').to_a
+  end
+
+  def numbers
+    ('1'..'26').to_a
+  end
+
+  def width_nums
+    numbers.first(@board_width)
+  end
+
+  def length_letters
+    alphabet.first(@board_height)
+  end
+
   def cell_maker
-    # sets array of nums and letters to potentially be used extend for coordinates
-    alphabet = ('A'..'Z').to_a
-    numbers = ('1'..'26').to_a
-    # sets the arrays of nums/letters to be used for coordinates
-    @width_nums = numbers.first(@board_width)
-    @length_letters = alphabet.first(@board_height)
-    # iterate through the num and letter arrays to build array of coordinates
-    @length_letters.each do |letter|
-      @width_nums.each do |num|
+    length_letters.each do |letter|
+      width_nums.each do |num|
         @array_of_coordinates << letter + num
       end
     end
-    # turn the array of coordinates into hash where key is coordinate and
-    # value is the cell
     @array_of_coordinates.each do |location|
       @cells[location] = Cell.new(location)
     end
@@ -53,56 +58,59 @@ attr_reader :cells,
     end
   end
 
-  def valid_placement?(ship, placement)
-    # build array of num array pairs
-    num_pairs = place_num(placement).each_cons(2).to_a
-    # create array checking each pair is consecutive
-    num_pairs_cons = num_pairs.map do |pair|
+  def num_pairs(placement)
+    place_num(placement).each_cons(2).to_a
+  end
+
+  def num_pairs_cons(placement)
+    num_pairs(placement).map do |pair|
       if pair[0] + 1 == pair[1]
         true
       else
         false
       end
     end
-    # turn num pairs into single bool confiriming if place_num is consecutive
-    nums_are_cons = if num_pairs_cons.uniq == [true]
-      true
-    else
-      false
-    end
-    # build array of letter pairs
-    alph_pairs = place_alph(placement).each_cons(2).to_a
-    # create array checking each pair of letters is consecutive
-    alph_pairs_cons = alph_pairs.map do |pair|
+  end
+
+  def alph_pairs(placement)
+    place_alph(placement).each_cons(2).to_a
+  end
+
+  def alph_pairs_cons(placement)
+    alph_pairs(placement).map do |pair|
       if pair[0].ord + 1 == pair[1].ord
         true
       else
         false
       end
     end
-    # turn alph pairs into single bool confiriming if place_alpha is consecutive
-    alphas_are_cons = if (alph_pairs_cons.uniq == [true])
-      true
-    else
-      false
-    end
-    # check to make sure all placements are empty
-    is_empty_location = placement.each do |cell|
+  end
+
+  def nums_are_cons(placement)
+    num_pairs_cons(placement).uniq == [true]
+  end
+
+  def alphas_are_cons(placement)
+    alph_pairs_cons(placement).uniq == [true]
+  end
+
+  def is_empty_location(placement)
+    placement.each do |cell|
       if @cells[cell].empty? == false
         return false
       end
     end
-    # take this branch if ship size doesn't match num of coordinates
-    if ship.length != placement.length || !is_empty_location
+  end
+
+  def valid_placement?(ship, placement)
+    if ship.length != placement.length || !is_empty_location(placement)
       false
-    # take this branch if all the letters are the same
     elsif place_alph(placement).uniq.length == 1
-        nums_are_cons
-      # take this branch if all nums are same
+      nums_are_cons(placement)
     elsif place_num(placement).uniq.length == 1
-        alphas_are_cons
-      else
-        false
+      alphas_are_cons(placement)
+    else
+      false
     end
   end
 
@@ -114,20 +122,14 @@ attr_reader :cells,
 
   def render(view_ships = false)
     board_visual = "  "
-    # iterate through our cell hash and render each cells
-    # every 4 cells do a new line and enter the appropriate letter first
-    # first print out column header nums and new line
-    @width_nums.each do |num|
+    width_nums.each do |num|
       board_visual += "#{num} "
     end
     board_visual += "\n"
     counter = 0
-    # print letter and 4 cells new line repeat for each letter
-    @length_letters.each do |letter|
+    length_letters.each do |letter|
       board_visual += "#{letter} "
       @board_width.times do
-        # access the array of coordinates take that to access the key in the Hash
-        # access the @view in the cell of that coordinate
         cell_key = @array_of_coordinates[counter]
         board_visual += "#{@cells[cell_key].render(view_ships)} "
         counter += 1
